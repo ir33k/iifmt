7fd34440b3686f78169d89c47f9c8c8d2647f870
irek <irek@gabr.pl>
2025-03-31 00:02:59 +0200

    Configure output with env variables

--- iifmt.c
+++ iifmt.c
@@ -4,20 +4,31 @@
 #include <time.h>
 
 int main() {
-	char time[32], msg[BUFSIZ];
+	char time[32], msg[BUFSIZ], *fmt="%H:%M", *env;
 	time_t timestamp;
 	struct tm *tm;
 	int i, len, max=72;
 
+	if ((env = getenv("IIFMT_FMT")))
+		fmt = env;
+
+	if ((env = getenv("IIFMT_MAX")))
+		max = atoi(env);
+
 	while (scanf("%ld %[^\n]s\n", &timestamp, msg) != EOF) {
 		tm = localtime(&timestamp);
-		strftime(time, sizeof time, "%H:%M", tm);
+		strftime(time, sizeof time, fmt, tm);
 		printf("%s %.*s\n", time, max, msg);
 
-		/* Wrap lines longer than MAX */
 		len = (int)strlen(msg);
+
+		/* Use time buffer for indentation */
+		for (i=0; time[i]; i++)
+			time[i] = ' ';
+
+		/* Wrap lines longer than MAX */
 		for (i = max; len - i > 0; i += max)
-			printf("      %.*s\n", max, msg+i);
+			printf("%s %.*s\n", time, max, msg+i);
 	}
 	return 0;
 }
